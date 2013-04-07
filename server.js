c5dc57d71d2f647f8640d89d0d310151b8357e8a
zxm5010 <zxm5010@gmail.com>
2013-04-07 18:43:30 -0300

    Update server.js
    Auth

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 var config    = require('./config');
 var flash     = require('connect-flash');
 var cons      = require('consolidate');
@@ -16,11 +15,76 @@ var redis     = require('redis').createClient();
 var app        = express();
 var RedisStore = require('connect-redis')(express);
 
+
+var FacebookStrategy = require('passport-facebook').Strategy;
+var TwitterStrategy = require('passport-twitter').Strategy;
+var GoogleStrategy = require('passport-google').Strategy;
+
+var FACEBOOK_APP_ID= "102295256632865";
+var FACEBOOK_APP_SECRET = "18bb50350b469fa3ae018acd6dc4fff9";
+
+var TWITTER_CONSUMER_KEY= "IEv4ktjCYIjMpYOWDRj0w";
+var TWITTER_CONSUMER_SECRET= "cnSUcInpbqdXVr9MYVEoMMuDfequXG0RmOp6Z3vlmo";
+
 // Set configuration context
 app.config = config[app.settings.env];
 
 // Connect to MongoDB
 var db = mongoose.connect(app.config.database.uri);
+
+// Auth setup
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+
+passport.use(new TwitterStrategy({
+    consumerKey: TWITTER_CONSUMER_KEY,
+    consumerSecret: TWITTER_CONSUMER_SECRET,
+    callbackURL: "http://127.0.0.1:3000/auth/twitter/callback"
+  },
+  function(token, tokenSecret, profile, done) {
+    process.nextTick(function () {
+      console.log("Username = " + profile.displayName);
+      return done(null, profile);
+    });
+  }
+));
+
+passport.use(new FacebookStrategy({
+    clientID: FACEBOOK_APP_ID,
+    clientSecret: FACEBOOK_APP_SECRET,
+    callbackURL: "http://localhost:3000/auth/facebook/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    process.nextTick(function () {
+      console.log("Username = " + profile.displayName);
+      return done(null, profile);
+    });
+  }
+));
+
+passport.use(new GoogleStrategy({
+    returnURL: 'http://localhost:3000/auth/google/return',
+    realm: 'http://localhost:3000/'
+  },
+  function(identifier, profile, done) {
+    // asynchronous verification, for effect...
+    process.nextTick(function () {
+      
+      // To keep the example simple, the user's Google profile is returned to
+      // represent the logged-in user. In a typical application, you would want
+      // to associate the Google account with a user record in your database,
+      // and return that user instead.
+      profile.identifier = identifier;
+      return done(null, profile);
+    });
+  }
+));
 
 app.configure(function(){
   app.set('port', process.env.PORT || app.config.port);
@@ -67,6 +131,51 @@ app.get('/', function(req, res, next) {
   });
 });
 
+
+//Facebook auth
+app.get('users/actions', function(req, res){
+  res.render('actions', { user: req.user });
+});
+app.get('/auth/facebook',
+  passport.authenticate('facebook'),
+  function(req, res){
+  });
+
+app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+
+//Twitter Auth
+app.get('/auth/twitter',
+  passport.authenticate('twitter', { failureRedirect: '/login'}),
+  function(req, res){
+    res.redirect('/');
+  });
+
+app.get('/auth/twitter/callback',
+  passport.authenticate('twitter', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+
+//Google Auth
+app.get('/auth/google',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+app.get('/auth/google/return',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
 app.server = http.createServer(app);
 
 helpers.loadModels(db);
